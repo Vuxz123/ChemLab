2b79633358451852586ac44d8843c95aaa1890b2
BigHamster <72179035+Vuxz123@users.noreply.github.com>
2024-10-14 14:32:32 +0700

    commit: AtomModel - Renderer (#1)

---
+++ Assets/com.ethnicthv/chemlab/client/api/model/IBondModel.cs
@@ -0,0 +1,10 @@
+﻿using com.ethnicthv.chemlab.engine;
+
+namespace com.ethnicthv.chemlab.client.api.model
+{
+    public interface IBondModel : IModel
+    {
+        public Bond GetBond();
+        public Bond.BondType GetBondType();
+    }
+}
---
+++ Assets/com.ethnicthv/chemlab/client/api/render/IRenderer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace com.ethnicthv.chemlab.client.api.render
+{
+    public interface IRenderer<T> where T : IRenderable
+    {
+        public void Render(T renderable, Camera camera);
+        
+        public void RenderGizmos(T renderable);
+    }
+}
