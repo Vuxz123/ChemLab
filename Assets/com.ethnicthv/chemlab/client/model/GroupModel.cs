f8f449bbf0a3e6c0bea52c0c2623cb8ae650db4e
Vuxz123 <ethnicthv123@gmail.com>
2024-10-16 12:08:22 +0700

    commit: Creating Custom Render Feature for URP

---
+++ Assets/com.ethnicthv/chemlab/client/model/GroupModel.cs
@@ -0,0 +1,14 @@
+﻿using com.ethnicthv.chemlab.client.api.model;
+using UnityEngine;
+
+namespace com.ethnicthv.chemlab.client.model
+{
+    public abstract class GroupModel : IModel
+    {
+        public abstract Mesh GetMesh();
+
+        public abstract Vector3 GetPosition();
+
+        public abstract Quaternion GetRotation();
+    }
+}
