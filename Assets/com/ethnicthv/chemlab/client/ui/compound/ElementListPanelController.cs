8e1bd16f1fb1ab3ae830332a507de88c436ade14
BigHamster <ethnicthv123@gmail.com>
2025-02-26 02:45:10 +0700

    commit: checkpoint 26/2/2035 2:45 AM

--- Assets/com/ethnicthv/chemlab/client/ui/compound/ElementListPanelController.cs
+++ Assets/com/ethnicthv/chemlab/client/ui/compound/ElementListPanelController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using com.ethnicthv.chemlab.client.api.ui.compound;
 using com.ethnicthv.chemlab.engine.api.element;
 using com.ethnicthv.util.pool;
